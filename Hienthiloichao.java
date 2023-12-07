d1b0cff205b4ca8bb97a23f9b453efe393dbd1ba
vuduyhn96 <vuanhduyhn1996@gmail.com>
2023-12-07 17:07:59 +0700

    hienthiloichao

---
+++ Hienthiloichao.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Hienthiloichao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập tên vào đây: ");
+        String ten = scanner.nextLine();
+        System.out.println("Xin chào, " + ten + "!");
+
+    }
+}
